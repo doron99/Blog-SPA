c23d07e59509e67a8d8b1050434575d19e5505c7
doron99 <maromdo@gmail.com>
2021-04-02 12:46:21 +0300

    minify code added

--- blog-spa/vue.config.js
+++ blog-spa/vue.config.js
@@ -1,3 +1,29 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
-    outputDir : '../../Blog-API/Blog-API/wwwroot/ClientApp'
+    outputDir : '../../Blog-API/Blog-API/wwwroot/ClientApp',
+    configureWebpack: config => {
+      config.optimization = {
+        minimize: true,
+        minimizer: [
+          new TerserPlugin({
+            terserOptions: {
+              ecma: undefined,
+              warnings: false,
+              parse: {},
+              compress: { drop_console: true },
+              mangle: true, // Note `mangle.properties` is `false` by default.
+              module: false,
+              output: { comments: false },
+              toplevel: false,
+              nameCache: null,
+              ie8: false,
+              keep_classnames: undefined,
+              keep_fnames: false,
+              safari10: false,
+            },
+          }),
+        ],
+      }
+    }
   }
